3d15e79ae8b7a0a1df10ddf6de8ab868f55cda2a
Tuna Tore <tuna.tore@cms.local>
2016-10-22 10:49:19 +0200

    Stream Custom sorted example

---
+++ src/main/java/java8/samples/stream/Person.java
@@ -0,0 +1,33 @@
+package java8.samples.stream;
+
+public class Person {
+
+	Person(int id, String name, int departmentId) {
+		this.id = id;
+		this.name = name;
+		this.departmentId = departmentId;
+	} 
+	
+	int id;
+	String name;
+	int departmentId;
+	 
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getDepartmentId() {
+		return departmentId;
+	}
+	public void setDepartmentId(int departmentId) {
+		this.departmentId = departmentId;
+	}		 
+}
---
+++ src/main/java/java8/samples/stream/StreamCustomSorted.java
@@ -0,0 +1,37 @@
+package java8.samples.stream;
+
+import java.util.Comparator;
+import java.util.stream.Stream;
+
+public class StreamCustomSorted {
+
+	public static void main(String[] args) {
+
+		//Single sorter
+		Comparator<Person> comparatorPerson = (p1, p2) -> Integer.compare(
+	            p1.getDepartmentId(), p2.getDepartmentId());
+		
+		Stream.of(new Person(1,"tuna",10),new Person(2,"tuna2",10),new Person(2,"tuna3",20),
+				new Person(3,"tuna4",30),new Person(4,"tuna5",30))
+			.sorted(comparatorPerson)
+				.forEach(person -> {					
+					System.out.println("department: " + person.getDepartmentId());	
+					System.out.println("person: " + person.getName());
+				});		
+						
+		//Multiple sorting
+		System.out.println("\nMultiple comparator sorting");
+		Comparator<Person> comparatorPersonId = (p1, p2) -> Integer.compare(
+	            p1.getId(), p2.getId());
+
+		Stream.of(new Person(1,"tuna",10),new Person(2,"tuna2",10),new Person(3,"tuna3",20),
+				new Person(4,"tuna4",30),new Person(5,"tuna5",30),new Person(6,"tuna6",10))
+			.sorted(comparatorPerson.thenComparing(comparatorPersonId))
+				.forEach(person -> {					
+					System.out.println("department: " + person.getDepartmentId() 
+							+ " person id: " + person.getId() + " person name: " + person.getName());	
+				});		
+		
+	}
+
+}
