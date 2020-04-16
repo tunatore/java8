13c82aad067d365afc2eefcfa0a541d36fb83c03
tuna <tunatore@gmail.com>
2020-04-17 01:38:46 +0200

    java 8 functional interfaces updates

--- src/main/java/java8/samples/functionalinterfaces/FunctionalInterfaceSamples.java
+++ src/main/java/java8/samples/functionalinterfaces/FunctionalInterfaceSamples.java
@@ -3,7 +3,9 @@ package java8.samples.functionalinterfaces;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Predicate;
+import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
 public class FunctionalInterfaceSamples {
@@ -13,7 +15,10 @@ public class FunctionalInterfaceSamples {
         functionalMethodConsumer(Arrays.asList(1, 2, 3, 4, 5, 100), consumerFunction);
         System.out.println();
         functionalMethodConsumer(Arrays.asList(1, 2, 3, 4, 5, 100), (Integer i) -> System.out.print("i: " + i + "->"));
+        System.out.println();
+        functionalMethodConsumer(Arrays.asList(1, 2, 3, 4, 5, 100), System.out::print);
 
+        //Predicate takes an argument and validates it based on the condition
         Predicate<Integer> greaterThan10 = (Integer i) -> i > 10;
         Predicate<Integer> lessThan250 = (Integer i) -> i < 250;
         System.out.println();
@@ -25,6 +30,22 @@ public class FunctionalInterfaceSamples {
                 .filter(greaterThan10.and(lessThan250))
                 .collect(Collectors.toList());
         System.out.println("\ncollectList: " + collectList);
+
+        //Function takes one argument and returns one argument
+        Function<Integer,Integer> function = (Integer i) -> i * 2;
+        Function<Integer,Integer> function2 = (Integer i) -> i * 10;
+        functionalMethod(Arrays.asList(1, 2, 3, 4, 5), function);
+        System.out.println();
+        functionalMethod(Arrays.asList(1, 2, 3, 4, 5), function.andThen(function2));
+
+        //Supplier
+        Supplier<String> supplier = () -> "123456";
+        System.out.println("\n" + supplier.get());
+    }
+    private static void functionalMethod(List<Integer> integerList, Function<Integer, Integer> function) {
+        for(int i : integerList) {
+            System.out.print(function.apply(i) + "->");
+        }
     }
     private static void functionalMethodConsumer(List<Integer> integerList, Consumer<Integer> consumerFunction) {
         for(int i : integerList) {
